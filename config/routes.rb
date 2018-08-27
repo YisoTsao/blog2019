fc6c24b32865a589b3d725ae61f50b23c631a45c
gary <yiso05255@gmail.com>
2018-08-27 18:50:44 +0800

    add ignore file Gemfile

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,6 @@ Rails.application.routes.draw do
   resources :contacts
   root to: 'posts#index'
 
-  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+  devise_for :users
 
 end
