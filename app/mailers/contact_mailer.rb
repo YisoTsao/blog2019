ba76f4610276480ac9351756275661ce70978b91
gary <yiso05255@gmail.com>
2018-09-03 23:17:13 +0800

    change contact mailer

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,5 +1,5 @@
 class ContactMailer < ApplicationMailer
-default from: "'Gary'yiso05255@gmail.com"
+default from: "'Gary'gary525t@gmail.com"
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
   #
