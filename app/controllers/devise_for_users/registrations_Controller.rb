194eb1c1dd8b7bdeeb4a28e49c66ab937807fa4b
gary <yiso05255@gmail.com>
2018-09-04 16:26:53 +0800

    modify registrations edit page

--- app/controllers/devise_for_users/registrations_Controller.rb
+++ app/controllers/devise_for_users/registrations_Controller.rb
@@ -1,14 +1,13 @@
 class DeviseForUsers::RegistrationsController < Devise::RegistrationsController
- 	
-  
+   before_action :reg_sign_up_params, if: :devise_controller?
+
   private
  
   def reg_sign_up_params
-    params.require(:users).permit(:account,:name,:click_id,:phone,:agree_terms, :id_number,
-    :birth_day, :country_code, :qq, :webchat)
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:account,:name,:click_id,:phone,:agree_terms, :id_number,
+    :birth_day, :country_code, :qq, :webchat])
+    #add below that can edit user profile
+    devise_parameter_sanitizer.permit(:account_update, keys: [:account,:name,:click_id,:phone,:agree_terms, :id_number,
+    :birth_day, :country_code, :qq, :webchat])
   end
- 
 end
-
-
-
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
          :confirmable
-		     
+  validates_acceptance_of :agree_terms
 end
