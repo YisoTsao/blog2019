8c73b6b43f1b38e5d7647cd8b1fe7daa3a340ae6
gary <yiso05255@gmail.com>
2018-09-05 01:39:43 +0800

    config/routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
-  
+
   mount Ckeditor::Engine => '/ckeditor'
-  resources :posts
+  resources :posts do
+    resources :comments
+  end
   resources :contacts
+  
   root to: 'posts#index'
 
   devise_for :users, controllers: { registrations: 'devise_for_users/registrations', passwords: 'devise_for_users/passwords', confirmations: 'devise_for_users/confirmations' }
